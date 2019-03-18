8463841c6ba3444d2715368c6611ee1f5e4759b2
thodoris <thodorisk@icloud.com>
2019-03-18 22:30:16 +0200

    Adding login functionality and jwt signing

--- api/routes/user.js
+++ api/routes/user.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
 
@@ -40,6 +41,45 @@ router.post('/signup', (req, res, next) => {
             });
         }
     })
+});
+
+router.post('/login', (req, res, next) => {
+    User.find({ username: req.body.username })
+    .exec()
+    .then(user => {
+        if (user.length < 1) {
+            return res.status(401).json({
+                message: 'Auth failed'
+            });
+        }
+        bcrypt.compare(req.body.password, user[0].password, (err, result) => {
+            if (err) {
+                return res.status(401).json({
+                    message: 'Auth failed'
+                });
+            }
+            if (result) {
+                const token = jwt.sign({
+                    username: user[0].password,
+                    userId: user[0]._id
+                }, 
+                process.env.JWT_KEY, 
+                {
+                    expiresIn: "1h"
+                });
+                return res.status(200).json({
+                    message: 'Auth successful',
+                    token: token
+                });
+            }
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err
+        });
+    });
 });
 
 module.exports = router;
